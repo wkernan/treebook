f046a1405524cdd99ea435a59cc976f33198d644
Bill Kernan <wkernan@gmail.com>
2013-03-17 20:41:44 -0500

    Add bootstrap and worked on layout

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -44,7 +44,7 @@ class StatusesController < ApplicationController
 
     respond_to do |format|
       if @status.save
-        format.html { redirect_to @status, notice: 'Status was successfully created.' }
+        format.html { redirect_to statuses_path, notice: 'Status was successfully created.' }
         format.json { render json: @status, status: :created, location: @status }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class StatusesController < ApplicationController
 
     respond_to do |format|
       if @status.update_attributes(params[:status])
-        format.html { redirect_to @status, notice: 'Status was successfully updated.' }
+        format.html { redirect_to statuses_path, notice: 'Status was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
